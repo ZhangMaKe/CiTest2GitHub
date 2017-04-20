953d8541ba9d71a306db693ff6ae6679c64ab434
Mark Faiers <mark.faiers@spdspark.com>
2017-04-20 16:57:24 +0100

    first commit

---
+++ CiTest2Website/CiTest2Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CiTest2Website.MvcApplication" Language="C#" %>
---
+++ CiTest2Website/CiTest2Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CiTest2Website.Startup))]
+namespace CiTest2Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
